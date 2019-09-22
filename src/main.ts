b5baf43baf08a21424470414e4937f79f9c43ad8
Tom de Bruijn <tom@tomdebruijn.com>
2019-09-22 17:34:15 +0200

    WIP

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,21 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput} from inside a container`);
+    const token = core.getInput('repo-token', {required: true});
+    const client = new github.GitHub(token);
+    core.debug(`Hello from inside a container`);
 
     // Get github context data
     const context = github.context;
     console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
+    const issue: {owner: string; repo: string; number: number} = context.issue;
+
+    await client.issues.createComment({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+      body: "This is a test"
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
