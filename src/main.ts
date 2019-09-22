8d348e1a84c2d7b4ee21497bbc937985cfe59393
Tom de Bruijn <tom@tomdebruijn.com>
2019-09-22 17:46:27 +0200

    fixup! WIP

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,14 @@ async function run() {
     const context = github.context;
     console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
     const issue: {owner: string; repo: string; number: number} = context.issue;
+    const message = "This is a test"
+    console.log(`Adding message: "${message}" to issue ${issue.owner}/${issue.repo}#${issue.number}`);
 
     await client.issues.createComment({
       owner: issue.owner,
       repo: issue.repo,
       issue_number: issue.number,
-      body: "This is a test"
+      body: message
     });
   } catch (error) {
     core.setFailed(error.message);
